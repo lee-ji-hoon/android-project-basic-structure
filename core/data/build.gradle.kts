ee4a667f9161e9d43edd34adb66bf2075f01ae7d
ezhoon <dlwlgns1240@gmail.com>
2023-07-26 22:52:56 +0900

    feat core:data

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("android-project-basic-structure.android.library")
+    id("android-project-basic-structure.android.hilt")
+}
+
+android {
+    namespace = "com.android_project_basic_structure.data"
+}
+
+dependencies {
+    // TODO model 만들어지면 넣어두기
+}

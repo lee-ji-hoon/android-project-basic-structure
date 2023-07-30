3582577fef4e381f6554ab4692c4ef4f06b6825c
ezhoon <dlwlgns1240@gmail.com>
2023-07-30 18:03:54 +0900

    data layer dependency 정리 및 bundle 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -10,6 +10,11 @@ android {
 dependencies {
     implementation(projects.core.model)
     implementation(projects.core.domain)
+    implementation(projects.library.test)
 
-    implementation(project(":library:test"))
+    implementation(libs.kotlin.serialization.json)
+    implementation(libs.kotlin.serialization.converter)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
 }
